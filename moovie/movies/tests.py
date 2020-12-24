9a8edba16e8c2a9ed498b230ed1e6748003a6664
Hassan-M <hassan.ma1245@gmail.com>
2020-12-24 22:59:11 +0330

    add tests for top rated, top release and movie api;join list with commas in django templates

--- moovie/movies/tests.py
+++ moovie/movies/tests.py
@@ -31,6 +31,29 @@ class MovieWithoutDBTestCase(TestCase):
         response = self.client.get('/api/Search/', data={'search': 'toy'})
         self.assertEqual(response.status_code, 500)
 
+    def test_movie_details_api_without_data(self):
+        response = self.client.get('/api/Movie/')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json(), {'status': 0})
+
+    def test_movie_details_api_with_data(self):
+        response = self.client.get('/api/Movie/1')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json(), {'status': 0})
+        response = self.client.get('/api/Movie/ad')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json(), {'status': 0})
+
+    def test_top_rated_api_without_data(self):
+        response = self.client.get('/api/TopRated/')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json(), {'movie_ids': []})
+
+    def test_release_date_api_without_data(self):
+        response = self.client.get('/api/ReleaseDate/')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json(), {'movie_ids': []})
+
 
 class MovieTestCase(TestCase):
     def setUp(self):
@@ -158,3 +181,57 @@ class MovieTestCase(TestCase):
         response = self.client.get('/api/Search/', data=data)
         self.assertEqual(response.status_code, 404)
 
+    def test_movie_details_api_with_loaded_data(self):
+        response = self.client.get('/api/Movie/')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json(), {'status': 0})
+
+    def test_movie_details_api_with_loaded_data(self):
+        response = self.client.get('/api/Movie/1')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(
+            response.json(), {
+                "id": 1,
+                "title": "toy story",
+                "casts": [
+                    "Tom"
+                ],
+                "writers": [
+                    "Joss Whedon"
+                ],
+                "directors": [
+                    "John Lasseter"
+                ],
+                "budget": 30000,
+                "genres": [
+                    "Animation",
+                    "Family"
+                ],
+                "language": "En",
+                "overview": "hi this is only for test",
+                "companies": "Pixar",
+                "countries": "USA",
+                "release_date": "1995-10-30",
+                "revenue": 3000000,
+                "runtime": 120,
+                "vote_average": 8.0,
+                "vote_count": 1000,
+                "poster": "https://image.tmdb.org/t/p/original/rhIRbceoE9lR4veEXuwCC2wARtG.jpg"
+            }
+        )
+        response = self.client.get('/api/Movie/12')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json(), {'status': 0})
+        response = self.client.get('/api/Movie/asdf')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json(), {'status': 0})
+
+    def test_top_rated_api_with_loaded_data(self):
+        response = self.client.get('/api/TopRated/')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json(), {'movie_ids': [1]})
+
+    def test_release_date_api_with_loaded_data(self):
+        response = self.client.get('/api/ReleaseDate/')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json(), {'movie_ids': [1]})
