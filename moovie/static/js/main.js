ac297d0888399fd695b09918359c9bcc0d0299e8
hb <hossein.bahak@gmail.com>
2020-12-28 23:51:13 +0330

    rate color circle

--- moovie/static/js/main.js
+++ moovie/static/js/main.js
@@ -2,3 +2,18 @@ $('#myTab a').on('click', function (e) {
     e.preventDefault()
     $(this).tab('show')
 })
+$('.movie-item__rate').each(function(rate){
+            var rate = $(this).html();
+            if($(this).hasClass('movie-item_rate--good')){$(this).removeClass('movie-item_rate--good');}
+            if($(this).hasClass('movie-item_rate--normal')){$(this).removeClass('movie-item_rate--normal');}
+            if($(this).hasClass('movie-item_rate--bad')){$(this).removeClass('movie-item_rate--bad');}
+            if(parseFloat(rate) > 7){
+                $(this).addClass('movie-item__rate--good');
+            }
+            else if(parseFloat(rate) > 5){
+                $(this).addClass('movie-item__rate--normal');
+            }
+            else{
+                $(this).addClass('movie-item__rate--bad');
+            }
+        });
